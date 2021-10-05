4b1e3faa2d9d96fbe206e748387b9ca734c2ae4f
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2021-10-05 12:46:03 -0300

    Arquivo do lab 4 de algoritmo
    O nome dos integrantes do grupo e a descrição da atividade já estão inclusas

---
+++ Algoritmo/Lab 4/divisibilidade.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Disciplina  : [Algoritimo e Lógica de Programação]
+// Professora  : Eliane Santiago
+// Turma       : 1° Desenvolvimento de Software Multiplataforma
+// Descrição   : Escrever um algoritmo/programa que receba dois valores inteiros positivos (x e y) e informar se x é divisível por y. 
+// Autor(a)    : Ana Claudia Rescia Royo Felippe - RA: 1111392121026
+// Autor(a)    : Evaristo Batista Tavares Esteves - RA: 1111392121008
+// Autor(a)    : Giovanna de Souza Almeida - RA: 1111392121031
+// Autor(a)    : Marco Royo Felippe - RA: 1111392121039
+// Data atual  : 05/10/2021
+
+int main() {
+    
+}
